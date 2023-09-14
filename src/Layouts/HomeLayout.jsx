8f201a46eca3c2c2af33082c63ab122aa7198b9c
KAMALAMAN687 <kamalaman687@gmail.com>
2023-09-14 11:55:02 +0530

    Added Logout Functionality

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -4,6 +4,7 @@ import { AiFillCloseCircle } from "react-icons/ai";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import Footer from "../Components/Footer";
 import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../Redux/Slices/AuthSlice";
 
 function HomeLayout({ children }) {
   const dispatch = useDispatch();
@@ -30,12 +31,11 @@ function HomeLayout({ children }) {
     drawerSide[0].style.width = "0";
   }
 
-  function handleLogout(e) {
+  async function handleLogout(e) {
     e.preventDefault();
 
-    //const res = await dispatch(logout());
-    //if (res?.payload?.success)
-    navigate("/");
+    const res = await dispatch(logout());
+    if (res?.payload?.success) navigate("/");
   }
 
   return (
