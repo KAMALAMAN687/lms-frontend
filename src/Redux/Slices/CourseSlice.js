e31819fb3ed7b330105a54ee683c0b28e2d4586c
KAMALAMAN687 <kamalaman687@gmail.com>
2023-09-16 17:24:52 +0530

    Added Course Create Page

--- src/Redux/Slices/CourseSlice.js
+++ src/Redux/Slices/CourseSlice.js
@@ -20,6 +20,28 @@ export const getAllCourses = createAsyncThunk("/course/get", async () => {
     toast.error(error?.response?.data?.message);
   }
 });
+export const createNewCourse = createAsyncThunk(
+  "/course/create",
+  async (data) => {
+    try {
+      let formData = new FormData();
+      formData.append("title", data?.title);
+      formData.append("description", data?.description);
+      formData.append("category", data?.category);
+      formData.append("createdBy", data?.createdBy);
+      formData.append("thumbnail", data?.thumbnail);
+      const response = axiosInstance.post("/courses", formData);
+      toast.promise(response, {
+        loading: "Creating New Course",
+        success: "Course Created Successfully",
+        error: "Failed to create Course",
+      });
+      return (await response).data;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  }
+);
 
 const courseSlice = createSlice({
   name: "courses",
