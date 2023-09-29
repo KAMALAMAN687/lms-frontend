66b7c1c7b51537eba9e81e15f4b080a383665029
KAMALAMAN687 <kamalaman687@gmail.com>
2023-09-29 15:17:37 +0530

    Edit Auth Slice

--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -5,7 +5,10 @@ import { toast } from "react-hot-toast";
 const initialState = {
   isLoggedIn: localStorage.getItem("isLoggedIn") || false,
   role: localStorage.getItem("role") || "",
-  data: JSON.parse(localStorage.getItem("data")) || {},
+  data:
+    localStorage.getItem("data") != undefined
+      ? JSON.parse(localStorage.getItem("data"))
+      : {},
 };
 
 export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
