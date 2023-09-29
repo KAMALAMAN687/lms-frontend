049449400a85c339b2f112bab0744081bd0c1a8e
KAMALAMAN687 <kamalaman687@gmail.com>
2023-09-29 21:45:42 +0530

    Added some Denied page button action

--- src/Pages/Denied.jsx
+++ src/Pages/Denied.jsx
@@ -10,7 +10,7 @@ function Denied() {
       <div className="bg-black text-white px-2 rext-sm rounded rotate-12 absolute ">
         Access Denied
       </div>
-      <button onClick={() => navigate(-1)} className="mt-5">
+      <button onClick={() => navigate("/")} className="mt-5">
         <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
           Go Back
         </span>
