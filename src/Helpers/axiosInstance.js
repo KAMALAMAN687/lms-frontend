dcca50d5e091f32707b334040d0d7cafe2255808
KAMALAMAN687 <kamalaman687@gmail.com>
2024-08-11 01:59:53 +0530

    api/v1

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://lms-backened.onrender.com/";
+const BASE_URL = "https://lms-backened.onrender.com/api/v1/";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
